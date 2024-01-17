baa0853f6630f65da23ade34ddadc04183d06646
Takeshi Tsukamoto <dev@itome.team>
2024-01-17 17:27:56 +0900

    Make apps list selectable

--- src/components/apps.rs
+++ src/components/apps.rs
@@ -28,7 +28,7 @@ impl AppsComponent {
             session_manager,
             daemon,
             devices: Arc::new(Mutex::new(vec![])),
-            list_state: ListState::default(),
+            list_state: ListState::default().with_selected(Some(0)),
             is_selected: false,
         }
     }
@@ -36,14 +36,62 @@ impl AppsComponent {
     pub fn set_selected(&mut self, is_selected: bool) {
         self.is_selected = is_selected;
     }
+
+    fn next(&mut self) {
+        let i = match self.list_state.selected() {
+            Some(i) => {
+                let sessions = &self.session_manager.lock().unwrap().sessions;
+                if i >= sessions.len() {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.list_state.select(Some(i));
+    }
+
+    fn previous(&mut self) {
+        let i = match self.list_state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    let sessions = &self.session_manager.lock().unwrap().sessions;
+                    sessions.len()
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.list_state.select(Some(i));
+    }
+
+    fn unselect(&mut self) {
+        self.list_state.select(None);
+    }
+
+    fn run_new_app(&mut self) -> Result<()> {
+        if let Ok(mut session_manager) = self.session_manager.lock() {
+            session_manager.run_new_app()?;
+        }
+        Ok(())
+    }
 }
 
 impl Component for AppsComponent {
     fn handle_key_events(&mut self, key: KeyEvent) -> Result<Option<Action>> {
-        if key.code == KeyCode::Char('n') {
-            if let Ok(mut session_manager) = self.session_manager.lock() {
-                session_manager.run_new_app()?;
+        match key.code {
+            KeyCode::Char('n') => {
+                self.run_new_app()?;
             }
+            KeyCode::Up => {
+                self.previous();
+            }
+            KeyCode::Down => {
+                self.next();
+            }
+            _ => {}
         }
         Ok(None)
     }
@@ -90,7 +138,6 @@ impl Component for AppsComponent {
 
         let block = Block::default()
             .title("Apps")
-            .padding(Padding::horizontal(1))
             .borders(Borders::ALL)
             .border_style(Style::default().fg(default_color));
 
@@ -99,18 +146,16 @@ impl Component for AppsComponent {
             .iter()
             .enumerate()
             .map(|(index, _)| {
-                ListItem::new(format!("App {}", index + 1))
+                ListItem::new(format!(" App {} ", index + 1))
                     .style(Style::default().fg(enabled_color))
             })
             .collect::<Vec<_>>();
-        items.push(ListItem::new("▶ Run new app").style(Style::default().fg(default_color)));
+        items.push(ListItem::new(" ▶ Run new app ").style(Style::default().fg(default_color)));
 
         let list = List::new(items)
             .block(block)
             .fg(Color::White)
-            .highlight_style(Style::new().add_modifier(Modifier::REVERSED))
-            .highlight_symbol(">>")
-            .repeat_highlight_symbol(true);
+            .highlight_style(Style::new().add_modifier(Modifier::REVERSED));
 
         f.render_stateful_widget(list, area, &mut self.list_state);
         Ok(())
